8350d9a59571a1bf58668cf88e72c63681ed46a2
Martin Dimitrov <martin3dimitrov@gmail.com>
2023-02-19 13:45:20 -0500

    utilize callbacks instead of blocking

--- vhs-notify.py
+++ vhs-notify.py
@@ -3,7 +3,6 @@ import os
 import random
 from PIL import Image
 import requests
-import time
 
 # ------
 delay = 10
@@ -22,13 +21,10 @@ def script_description():
   This function will force OBS to screenshot the current preview, then load it into memory and delete the fs screenshot
 """
 def extract_screenshot():
-  # potential race condition? I don't know if this is blocking call
-  obs.obs_frontend_take_screenshot()
+  # this should be called after the callback from a successful screenshot
   fp = obs.obs_frontend_get_last_screenshot()
 
-  # blocking until the screenshot is created meh
-  while not os.path.exists(fp):
-    time.sleep(1)
+  print("Loading image at: ", fp)
 
   # open the ss and store FULLY in memory
   img = Image.open(fp)
@@ -45,8 +41,8 @@ This function will select n random pixels from the center/upper line of the scre
 def get_random_central_pixels(n: int, w: int, h: int) -> list:
   ps = []
   for _ in range(n):
-    x = random.randint(w)
-    y = random.randint(h)
+    x = random.randint(0, w)
+    y = random.randint(0, h)
 
     ps.append((x, y))
 
@@ -59,6 +55,44 @@ def is_blueish(p: tuple) -> bool:
 def is_static(p: tuple) -> bool:
   # approx check for static 
   return p[0] == p[1] and p[1] == p[2]
+
+def ss_check_callback():
+  ss = extract_screenshot()
+
+  ps = get_random_central_pixels(pixels, ss.size[0], ss.size[1])
+  isEndBlue = True
+  isStatic = True
+  
+  # check if all pixels are blueish -> signalling end of tape
+  for p in ps:
+    if not is_blueish(ss.getpixel(p)):
+      isEndBlue = False
+      break
+
+  # check if all static
+  for p in ps:
+    if not is_static(ss.getpixel(p)):
+      isStatic = False
+      break
+
+  if obs.obs_frontend_recording_active() and isEndBlue:
+    requests.post(ntfy_server, data="VHS Recording hit End Blue; stopping recording")
+    print("notify end blue; stopping recording")
+    obs.obs_frontend_recording_stop()
+    return
+
+
+  if obs.obs_frontend_recording_active() and isStatic:
+    requests.post(ntfy_server, data="VHS Recording hit Static; stopping recording")
+    print("notify isstatic, stop recording")
+    obs.obs_frontend_recording_stop()
+    return
+  
+  if not obs.obs_frontend_recording_active():
+    requests.post(ntfy_server, data="VHS Recording Starting up again")
+    print("notify recording start, start recording")
+    obs.obs_frontend_recording_start()
+
 
 # Called at script unload
 def script_unload():
@@ -90,42 +124,16 @@ def script_update(settings):
 # Global animation activity flag
 is_active = False
 
-def ss_check_callback():
-  ss = extract_screenshot()
-
-  ps = get_random_central_pixels(pixels, ss.size[0], ss.size[1])
-  isEndBlue = True
-  isStatic = True
-  
-  # check if all pixels are blueish -> signalling end of tape
-  for p in ps:
-    if not is_blueish(ss.getpixel(p)):
-      isEndBlue = False
-      break
-
-  # check if all static
-  for p in ps:
-    if not is_static(ss.getpixel(p)):
-      isStatic = False
-      break
-
-  if isEndBlue:
-    requests.post(ntfy_server, data="VHS Recording hit End Blue; stopping recording")
-    print("notify end blue; stopping recording")
-    obs.obs_frontend_recording_stop()
-    return
+def trigger_screenshot():
+  print("asking frontend to take a screenshot")
+  obs.obs_frontend_take_screenshot()
 
 
-  if isStatic:
-    requests.post(ntfy_server, data="VHS Recording hit Static; stopping recording")
-    print("notify isstatic, stop recording")
-    obs.obs_frontend_recording_stop()
-    return
-  
-  if not obs.obs_frontend_recording_active():
-    requests.post(ntfy_server, data="VHS Recording Starting up again")
-    print("notify recording start, start recording")
-    obs.obs_frontend_recording_start()
+def frontend_event_handler(data):
+    if data == obs.OBS_FRONTEND_EVENT_SCREENSHOT_TAKEN:
+      print("screenshot callback received, calling next step")
+      ss_check_callback()
+
 
 # Callback for the hotkey
 def on_auto_record_hotkey(pressed):
@@ -136,10 +144,11 @@ def on_auto_record_hotkey(pressed):
     print("VHS Auto recording: ", is_active)
     
     if is_active:
-      timer_add(ss_check_callback, delay * 1000)
-      obs.handler
+      obs.timer_add(trigger_screenshot, delay * 1000)
+      obs.obs_frontend_add_event_callback(frontend_event_handler)
     else:
-      timer_remove(ss_check_callback)
+      obs.timer_remove(trigger_screenshot)
+      obs.obs_frontend_remove_event_callback(frontend_event_handler)
 
 # Identifier of the hotkey set by OBS
 hotkey_id = obs.OBS_INVALID_HOTKEY_ID
